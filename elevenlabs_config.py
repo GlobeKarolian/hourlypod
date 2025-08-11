2bb18737eb2922b7c1e8dfdc29f306b2e49685b5
GlobeKarolian <matt.karolian@globe.com>
2025-08-11 02:48:31 -0400

    Update elevenlabs_config.py

--- elevenlabs_config.py
+++ elevenlabs_config.py
@@ -1,7 +1,6 @@
-# ElevenLabs Cadillac Voice Settings
-STABILITY = 0.62
-SIMILARITY_BOOST = 0.88
-STYLE = 0.48
-USE_SPEAKER_BOOST = True
-VOICE_SPEED = 1.04
-MODEL_ID = "eleven_v3"
+STABILITY = 0.90
+SIMILARITY_BOOST = 0.75
+STYLE = 0.10
+USE_SPEAKER_BOOST = False  # Try turning this off
+VOICE_SPEED = 1.0
+MODEL_ID = "eleven_multilingual_v2"
